3893e393b8b4b7dc1e673dfec5f6b3522fcefc87
labib-hossain23 <labib.hossain@ufl.edu>
2023-10-31 12:36:13 -0400

    second part of code

--- Lab6git.py
+++ Lab6git.py
@@ -1,4 +1,4 @@
-def main():
+def menu():
     print("Menu")
     print("-------------")
     print("1. Encode")
@@ -6,9 +6,9 @@ def main():
     print("3. Quit")
     global user_input
     user_input = 1
-    user_inuput = int(input("Please enter an option: "))
+    user_input = int(input("Please enter an option: "))
 
-    return user_inuput
+    return user_input
 
 def encode():
     password = input("Please enter your password to encode: ")
@@ -21,14 +21,27 @@ def encode():
     for i in range(0, len(list)):
         encoded_password = encoded_password + list[i]
     print("Your password has been encoded and stored!")
-    print(encoded_password)
 
     return encoded_password
+
+def decode_password(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_password += decoded_digit
+    return decoded_password
+def main():
+    while True:
+        user_input = menu()
+        if user_input == 1:
+            encoded_password = encode()
+            continue
+        elif user_input == 2:
+            decoded_password = decode_password(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+        elif user_input == 3:
+            break
 
 
 if __name__ == "__main__":
     main()
-    if user_input == 1:
-        encode()
-    elif user_input == 2:
-        print()
